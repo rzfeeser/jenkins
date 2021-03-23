2a2253256e98a1103a1f48292089127fcc2eaecf
MonaLisa <rzfeeser@users.noreply.github.com>
2021-03-23 00:58:29 -0400

    Create python01.py

---
+++ scripts/python01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A simple Python script || rzfeeser@alta3.com"""
+
+
+def main():
+
+    # this will be displayed to standard out
+    print("You have 13 hours to solve the labyrinth...")
+    
+if __name__ == "__main__":
+    main()
