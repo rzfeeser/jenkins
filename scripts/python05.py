8770e71bae9ea704c61742fd0ee38de88dac5a48
MonaLisa <rzfeeser@users.noreply.github.com>
2021-04-21 02:31:20 -0400

    Create python05.py

---
+++ scripts/python05.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Russell Zachary Feeser || rzfeeser@alta3.com"""
+
+# std library imports
+import json
+
+def main():
+    with open(onISS.txt) as f:
+        astros = json.load(f)
+    print(astros)
+        
+        
+if __name__ == "__main__":
+    main()
