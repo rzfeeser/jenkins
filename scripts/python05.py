3ebbc64b26533d35fadc56de612b9ca6bca94c8e
MonaLisa <rzfeeser@users.noreply.github.com>
2021-04-21 02:45:38 -0400

    Update python05.py

--- scripts/python05.py
+++ scripts/python05.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 """Russell Zachary Feeser || rzfeeser@alta3.com"""
 
-# std library imports
-import json
-
 def main():
     with open("onISS.txt") as f:
-        #print(f.read())
-        data = json.load(f)
-        print(data)
-        
+        print(f.read())
         
 if __name__ == "__main__":
     main()
